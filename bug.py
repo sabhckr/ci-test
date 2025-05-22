66327603ecd23f97af0df82e77c45c3752cdb360
sabhckr <142271082+sabhckr@users.noreply.github.com>
2025-05-22 15:07:58 +0600

    Update bug.py

--- bug.py
+++ bug.py
@@ -1,10 +1,12 @@
 # bug.py
 def divide(a, b):
+    if b == 0:
+        return "Error: Cannot divide by zero"
     return a / b
 
 def main():
     x = 10
-    y = 0  # bug: zero division error hobe ekhane
+    y = 0
     print("Result:", divide(x, y))
 
 if __name__ == "__main__":
